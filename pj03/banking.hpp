ccb677528c5b917756f08689c7f39de49af9d72f
Unagi_zoso <Unagi.zoso@gmail.com>
2022-04-25 23:37:51 +0900

    복사생성자 추가, Name 관련 상수 enum으로 변경

---
+++ pj03/banking.hpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits.h>
+#pragma once
+
+enum { ksign = 1, kDeposit, kWithdraw, kShow_All, kExit } e_Num_Select;
+enum { kMaxNameSize = 20 } e_Name_Const;
+typedef int Num_Select;
+
+class AccountInfo
+{
+public:
+    AccountInfo() = default;
+    AccountInfo(unsigned& current_back);
+    AccountInfo(AccountInfo& original_obj);
+    
+    virtual ~AccountInfo();
+    
+    unsigned ReturnAccountId() const;
+    std::string ReturnAccountName() const;
+    unsigned long long ReturnAccountMoney() const;
+
+    void InputAccountId(unsigned& account_id_input);
+    void InputAccountName(std::string& account_name_input);
+    void InputAccountMoney(unsigned long long& account_money_input);
+   
+    void IncreaseAccountMoney(unsigned long long& money_to_increase); 
+    void DecreaseAccountMoney(unsigned long long& money_to_decrease);
+
+    static AccountInfo& MatchId(std::vector<AccountInfo> & account_list, unsigned id_match);
+    
+    static unsigned GetId();
+    
+    void DepositAccount();
+
+    void WithdrawAccount();
+    
+    void ShowAccountInfo() const; 
+
+private:
+    unsigned account_id;
+    std::string account_name;
+    unsigned long long account_money;
+    static unsigned g_current_back;
+};
+
+void ViewMenu();
+
+void SelectMenuNum(unsigned & num_select);
+
+void SignInAccount(std::vector<AccountInfo>& account_list, unsigned& current_back);
---
+++ pj03/main.cpp
@@ -0,0 +1,35 @@
+#include "banking.hpp"
+
+int main()
+{
+	std::vector<AccountInfo> account_list(100);
+	unsigned num_select = 0;
+	unsigned current_back = 0;
+
+	while (1)
+	{
+		ViewMenu();
+		SelectMenuNum(num_select);
+		
+		switch (num_select)
+		{
+		case ksign: 
+       		account_list.at(current_back) = AccountInfo(current_back);
+			break;
+		case kDeposit:
+            AccountInfo::MatchId(account_list, AccountInfo::GetId()).DepositAccount();
+			break;
+		case kWithdraw:
+            AccountInfo::MatchId(account_list, AccountInfo::GetId()).WithdrawAccount();
+			break;
+		case kShow_All:
+            AccountInfo::MatchId(account_list, AccountInfo::GetId()).ShowAccountInfo();
+			break;
+		case kExit:
+			return 0;
+		default:
+			break;
+		} 
+	}
+	return 0;
+}
